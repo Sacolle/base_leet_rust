e07e134fbb592e11120b6080fe3c7dad56f37cbe
Pedro Colle <pedro.h.b.colle@gmail.com>
2022-05-26 21:09:01 -0300

    added template lang for cargo-generate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
+#[allow(dead_code)]
+struct Solution;
+
+
+#[allow(dead_code,non_snake_case)]
+impl Solution{
+
+}
+
+
 #[cfg(test)]
 mod tests {
+	use super::*;
     #[test]
-    fn it_works() {
+    fn base() {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
